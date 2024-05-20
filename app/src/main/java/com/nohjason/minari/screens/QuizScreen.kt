5261de4b06c3fe00e562068ca18c762f1c87a211
wlals6691 <127966468+wlals6691@users.noreply.github.com>
2024-05-20 23:47:41 +0900

    add: view2

--- app/src/main/java/com/nohjason/minari/screens/QuizScreen.kt
+++ app/src/main/java/com/nohjason/minari/screens/QuizScreen.kt
@@ -1,8 +1,6 @@
 package com.nohjason.minari.screens
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -14,7 +12,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusRequester.Companion.createRefs
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
@@ -23,7 +20,6 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.constraintlayout.widget.ConstraintLayout
 import com.nohjason.minari.R
 
 @OptIn(ExperimentalComposeUiApi::class)
@@ -31,6 +27,8 @@ import com.nohjason.minari.R
 @Composable
 fun QuizScreen_main() {
     Column {
+        
+        
         //폰트
         val pretendardFamily = FontFamily(
             Font(R.font.pretendard_regular, FontWeight.Thin),
@@ -38,24 +36,36 @@ fun QuizScreen_main() {
             Font(R.font.pretendard_semibold, FontWeight.SemiBold),
             Font(R.font.pretendard_bold, FontWeight.Bold)
         )
+        val a = 100.toString()
+
+        Text(
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            text = "My 포인트",
+            fontSize = 20.sp,
+            color = Color.White,
+            fontFamily = pretendardFamily,
+        )
 
         //첫번째 텍스트
         Text(
             text = "퀴즈를 시작하겠습니다!",
             modifier = Modifier
-                .padding(top = 400.dp)
-            ,
+                .padding(top = 194.dp)
+                .align(Alignment.CenterHorizontally),
             textAlign = TextAlign.Center,
             fontSize = 30.sp,
 //                fontWeight = FontWeight.SemiBold,
             fontFamily = pretendardFamily,
-            color = Color.Black,
+            color = Color.Black
         )
 
         Text(
             text = "총 10문제이며 O/X 형식으로 이루어져있습니다.",
             textAlign = TextAlign.Center,
-            modifier = Modifier.fillMaxWidth().padding(top = 193.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 193.dp)
+                .align(Alignment.CenterHorizontally),
             fontWeight = FontWeight.Medium,
             fontFamily = pretendardFamily,
             color = Color(0xFF363CD5)
@@ -66,7 +76,10 @@ fun QuizScreen_main() {
         Button(
             onClick = { /* TODO */ },
             modifier = Modifier
-                .align(Alignment.Center).width(310.dp).height(46.dp),
+                .padding(top = 30.dp)
+                .align(Alignment.CenterHorizontally)
+                .width(310.dp)
+                .height(46.dp),
             colors = ButtonDefaults.buttonColors(
                 containerColor = Color(0xFF363CD5)
             )
