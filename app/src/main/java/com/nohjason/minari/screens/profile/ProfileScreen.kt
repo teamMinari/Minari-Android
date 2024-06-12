42a17136dc07a47c7f7da164bffd130c2a071c6d
wlals6691 <127966468+wlals6691@users.noreply.github.com>
2024-06-12 12:24:10 +0900

    update

--- app/src/main/java/com/nohjason/minari/screens/profile/ProfileScreen.kt
+++ app/src/main/java/com/nohjason/minari/screens/profile/ProfileScreen.kt
@@ -1,14 +1,20 @@
 package com.nohjason.minari.screens.profile
 
 import android.net.Uri
+import android.provider.UserDictionary.Words
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
@@ -28,33 +34,90 @@ import coil.size.Scale
 @Preview
 @Composable
 fun ProfileScreen() {
+    Column (modifier = Modifier.fillMaxSize()){
+        Profile_box()
+        Text(text ="포인트")
+        Text(text = "1444")
+        Contents_box()
+        Shop_box()
+        MyWord_box()
+    }
+}
+
+@Composable
+fun Profile_box(){
     var imageUri by remember { mutableStateOf("") }
     val launcher = rememberLauncherForActivityResult(contract =
     ActivityResultContracts.GetContent()) { uri: Uri? ->
         imageUri = uri.toString()
     }
-
-    Column {
-        Text(text = "프로필을 선택하세요")
-        IconButton(onClick = {
-            launcher.launch("image/*")
-        }) {
-            Icon(
-                imageVector = Icons.Default.Phone,
-                contentDescription = "Search Profile",
-            )
+    Column (){
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "로그아웃")
         }
+        Row {
+            Text(text = "별명")
+            Text(text = "이름")
+            Text(text = "메일")
+            Text(text = "선택 카테고리")
+            Canvas(modifier = Modifier) {
 
-        Image(
-            painter = rememberImagePainter(
-                data = Uri.parse(imageUri),
-                builder = {scale(Scale.FIT)}
-            ),
-            contentDescription = null,
-            modifier = Modifier
-                .size(100.dp)
-                .clip(CircleShape),
-            contentScale = ContentScale.FillWidth,
-        )
+            }
+            Image(
+                painter = rememberImagePainter(
+                    data = Uri.parse(imageUri),
+                    builder = {scale(Scale.FIT)}
+                ),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(100.dp)
+                    .clip(CircleShape),
+                contentScale = ContentScale.FillWidth,
+            )
+            IconButton(onClick = {
+                launcher.launch("image/*")
+            }) {
+                Icon(
+                    imageVector = Icons.Default.Phone,
+                    contentDescription = "Search Profile",
+                )
+            }
+        }
     }
+}
+
+@Composable
+fun Contents_box(){
+    Box(modifier = Modifier){
+        Row {
+            Column {
+                IconButton(onClick = { /*TODO*/ }) {
+                    
+                }
+                Text(text = "단어장")
+            }
+            Column {
+                IconButton(onClick = { /*TODO*/ }) {
+
+                }
+                Text(text = "상점")
+            }
+            Column {
+                IconButton(onClick = { /*TODO*/ }) {
+
+                }
+                Text(text = "내역")
+            }
+        }
+    }
+}
+
+@Composable
+fun Shop_box(){
+    Text(text = "아직 개발 중")
+}
+
+@Composable
+fun MyWord_box(){
+    Text(text = "아직 개발 중")
 }
